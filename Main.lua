f7796e60e0a752a7c5259dff5f4426bf6dfd838c
Seakui <117586463+Seakui@users.noreply.github.com>
2022-11-06 16:58:55 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,6 @@
 -- dont judge :rage:
+
+
+if game.PlaceId == 10905034443 then -- Smoothie Factory Tycoon
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/Seakui/Games/main/SFT.lua'))()
+end
