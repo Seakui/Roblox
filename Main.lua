0b77af421e51d18599ea9b833494ae7f5a375f77
Seakui <117586463+Seakui@users.noreply.github.com>
2022-11-06 17:21:53 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,24 @@
 -- dont judge :rage:
 
 
+if not(game:IsLoaded()) then
+	game.Loaded:Wait()
+end
+
+-- antiafk
+local VirtualUser=game:service'VirtualUser'
+game:service'Players'.LocalPlayer.Idled:connect(function()
+VirtualUser:CaptureController()
+VirtualUser:ClickButton2(Vector2.new())
+end)
+
+
+
+
+
+-- games
+
+
 if game.PlaceId == 10905034443 then -- Smoothie Factory Tycoon
   loadstring(game:HttpGet('https://raw.githubusercontent.com/Seakui/Games/main/SFT.lua'))()
 end
