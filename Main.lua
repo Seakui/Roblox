5f7547db6221aa9136d8556e5fce93588056544a
Seakui <117586463+Seakui@users.noreply.github.com>
2022-11-16 22:22:35 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -23,8 +23,9 @@ end)
 
 if game.PlaceId == 10905034443 then -- Smoothie Factory Tycoon
   loadstring(game:HttpGet('https://raw.githubusercontent.com/Seakui/Games/main/SFT.lua'))()
-end
-
-if game.PlaceId == 4042427666 or game.PlaceId == 5113678354 or game.PlaceId == 5113680396 or game.PlaceId == 5445525505 or game.PlaceId == 6479720355 then -- Anime Fighting Simulator (Dimensions 1, 2, 3, 4, 5)
+elseif game.PlaceId == 4042427666 or game.PlaceId == 5113678354 or game.PlaceId == 5113680396 or game.PlaceId == 5445525505 or game.PlaceId == 6479720355 then -- Anime Fighting Simulator (Dimensions 1, 2, 3, 4, 5)
   loadstring(game:HttpGet('https://raw.githubusercontent.com/Seakui/Games/main/AFSBOSSIMMUNITY.lua'))()
+	  else
+      warn("[!] Seakui: Game not supported")
+game.StarterGui:SetCore( "ChatMakeSystemMessage",  { Text = "Looks like this game isn't supported yet.", Color = Color3.fromRGB(252, 3, 3), Font = Enum.Font.SourceSansBold, FontSize = Enum.FontSize.Size24 })
 end
