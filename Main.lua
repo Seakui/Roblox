a590cb0f084cced83150bb23152d68cfa9adfa94
Seakui <117586463+Seakui@users.noreply.github.com>
2023-07-29 20:02:26 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,8 +1,5 @@
 -- dont judge :rage:
-warn("[!] Executed!")
-
-local plr = game.Players.LocalPlayer
-print("Welcome back, "..plr.Name..".")
+print("Welcome back, "..game.Players.LocalPlayer.Name..".")
 
 -- load
 if not(game:IsLoaded()) then
@@ -21,13 +18,7 @@ if game.PlaceId == 10905034443 then -- Smoothie Factory Tycoon
   loadstring(game:HttpGet('https://raw.githubusercontent.com/Seakui/Games/main/SFT.lua'))()
 elseif game.PlaceId == 4042427666 or game.PlaceId == 5113678354 or game.PlaceId == 5113680396 or game.PlaceId == 5445525505 or game.PlaceId == 6479720355 then -- Anime Fighting Simulator (Dimensions 1, 2, 3, 4, 5)
   loadstring(game:HttpGet('https://raw.githubusercontent.com/Seakui/Games/main/AFSBOSSIMMUNITY.lua'))()
-elseif game.PlaceId == 11542692507 then -- Anime Souls Simulator
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/Seakui/Games/main/ASS.lua'))()
-elseif game.PlaceId == 12585099889 then -- spes
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/Seakui/Games/main/Spes.lua'))()
-elseif game.PlaceId == 9558874729 then -- super human training simulator
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/Seakui/Games/main/SHTS.lua'))()
-	else
+else
   warn("[!]: Game not supported")
 game.StarterGui:SetCore( "ChatMakeSystemMessage",  { Text = "Looks like this game isn't supported yet.", Color = Color3.fromRGB(252, 3, 3), Font = Enum.Font.SourceSansBold, FontSize = Enum.FontSize.Size24 })
 end
